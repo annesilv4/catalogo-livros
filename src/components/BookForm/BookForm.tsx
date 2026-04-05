4ce77ed8d50ebc4000abfdf40693414c3af07ca7
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-04-05 13:08:21 -0300

    feat: adiciona componente BookForm para cadastro de livros

---
+++ src/components/BookForm/BookForm.tsx
@@ -0,0 +1,55 @@
+import type { BookStatus } from "../../types/Book";
+import { useState } from "react";
+
+interface BookFormProps {
+    onAdd: (book: {
+        title: string;
+        author: string;
+        status: BookStatus;
+    }) => void;
+}
+
+export default function BookForm({ onAdd }: BookFormProps) {
+    const [author, setAuthor] = useState("");
+    const [title, setTitle] = useState("");
+    const [status, setStatus] = useState<BookStatus>("unread");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!author || !title) {
+            return;
+        }
+
+        onAdd({ title, author, status });
+
+        setAuthor("");
+        setTitle("");
+        setStatus("unread");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Título"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <input
+                type="text"
+                placeholder="Autor"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+            />
+
+            <select value={status} onChange={(e) => setStatus(e.target.value as BookStatus)}>
+                <option value="unread">Não Lido</option>
+                <option value="read">Lido</option>
+            </select>
+
+            <button type="submit">Adicionar Livro</button>
+        </form>
+    )
+}
