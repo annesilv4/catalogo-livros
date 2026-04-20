5c3fbd1f59637c6f797708f9f8ab10196def899c
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-04-20 18:31:17 -0300

    feat: adiciona página para adicionar livros com layout e estilo

---
+++ src/pages/books/page.tsx
@@ -0,0 +1,34 @@
+import Header from "../../components/Header/Header";
+import Style from "./AddBookPage.module.css";
+import BookForm from "../../components/BookForm/BookForm";
+import { Container } from "../../components/Container/Container";
+import { useBooks } from "../../hooks/useBooks";
+import { useEffect } from "react";
+
+export default function AddBookPage() {
+    useEffect(() => {
+        document.title = "Catálogo de Livros | Adicionar Livro";
+    }, []);
+
+    const { addBook } = useBooks();
+
+    return (
+        <>
+            <Header
+                ShowSearch={false}
+                showNav={false}
+            />
+
+            <main className="bg-gray-300 min-h-[calc(100vh-72px)]">
+                <Container className="bg-white p-10 h-230">
+                    <div className={Style.container}>
+                        <h2 className="text-2xl w-full font-bold text-gray-800 text-start pb-3 mb-6 border-b-2 border-yellow-500">
+                            Adicionar Livro
+                        </h2>
+                        <BookForm onAdd={addBook} />
+                    </div>
+                </Container>
+            </main>
+        </>
+    )
+}
