bb43eaa33a69166eb6705a398fac039cb0cb40e0
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-04-20 13:43:29 -0300

    feat: adiciona componente Header com estilos e navegação

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { Container } from "../Container/Container";
+import Style from "./Header.module.css";
+import Logo from "../../../public/assets/livro.png"
+
+export default function Header({ ShowSearch = true, showNav = true }: { ShowSearch?: boolean, showNav?: boolean }) {
+    return (
+        <header className={Style.header}>
+            <Container>
+                <div className={Style.headerInner}>
+                    <div className={Style.headerLogo}>
+                        <img src={Logo} className="w-10 h-10" alt="" />
+                        <h1>My Library</h1>
+                    </div>
+
+                    {ShowSearch && (
+                        <div className={Style.search}>
+                            <input type="text" placeholder="Buscar livro..." />
+                        </div>
+                    )}
+
+                    {showNav && (
+                        <nav id={Style.nav}>
+                            <a href="">Início</a>
+                            <a href="">Catálogo</a>
+                            <a href="">Favoritos</a>
+                        </nav>
+                    )}
+                </div>
+            </Container>
+        </header>
+    )
+}
